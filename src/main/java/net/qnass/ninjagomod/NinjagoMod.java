8cc1e7d23b68b9f0f143df3f600f1d9f123bcf1e
Anass <kay_oge@outlook.com>
2024-12-27 22:07:18 +0100

    added important comment

--- src/main/java/net/qnass/ninjagomod/NinjagoMod.java
+++ src/main/java/net/qnass/ninjagomod/NinjagoMod.java
@@ -5,6 +5,7 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+// very important comment
 public class NinjagoMod implements ModInitializer {
 	public static final String MOD_ID = "ninjagomod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
